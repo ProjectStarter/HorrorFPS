c15fd2cbd1326e8b62d361ff100007fc6dc9f6bf
zarasatelawrence <zarasatelawrence@gmail.com>
2015-07-14 15:39:24 +0800

    created the Scenes that will be use.

---
+++ Assets/Game/Scripts/Utils/SceneNames.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneName {
+	public const string SPLASH_SCENE_NAME = "SplashScene";
+	public const string MAINMENU_SCENE_NAME = "MainMenuScene";
+	public const string GAME_SCENE_NAME = "GameScene";
+}
